60390659aec576a8a901c34b254b31c82f86d21b
Doug Skinner <dskinne12@gmail.com>
2018-01-20 23:46:01 -0500

    Added another server

--- fabfile.py
+++ fabfile.py
@@ -38,6 +38,7 @@ def prep():
 def sync():
     local("rsync -avz --progress _site/* root@blog:/var/www/html")
     local("rsync -avz --progress _site/* root@blog2:/var/www/html")
+    local("rsync -avz --progress _site/* root@blog3:/var/www/html")
 
 def deploy():
     prep()
