b7251ae13a939f5a4258b75b3a4b709c8de5a893
Doug Skinner <dskinne12@gmail.com>
2018-04-12 14:02:42 -0400

    added dark mode by default

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,6 +4,50 @@ $(document).ready(function() {
   	$('[data-toggle="tooltip"]').tooltip()
 	});
 
+	$(function () {
+		$('#wrapper').addClass('dark');
+
+		$("#mode_toggle").html('Dark Mode');
+		$("#mode_toggle").attr('data-mode', 'dark');
+		$("#mode_toggle").removeClass('btn-light').addClass('btn-dark');
+
+		$('nav').removeClass('bg-light').removeClass('navbar-light').addClass('bg-dark').addClass('navbar-dark');
+
+		$('.card').addClass('dark');
+		$('.card-title').addClass('dark');
+		$('.card-text').addClass('dark');
+	});
+
+	$("#mode_toggle").on("click", function(e) {
+		var mode = $(this).attr('data-mode');
+
+		if (mode == 'light') {
+			$('#wrapper').addClass('dark');
+
+			$(this).html('Dark Mode');
+			$(this).attr('data-mode', 'dark');
+			$(this).removeClass('btn-light').addClass('btn-dark');
+
+			$('nav').removeClass('bg-light').removeClass('navbar-light').addClass('bg-dark').addClass('navbar-dark');
+
+			$('.card').addClass('dark');
+			$('.card-title').addClass('dark');
+			$('.card-text').addClass('dark');
+		} else {
+			$('#wrapper').removeClass('dark');
+
+			$(this).html('Light Mode');
+			$(this).attr('data-mode', 'light');
+			$(this).removeClass('btn-dark').addClass('btn-light');
+
+			$('nav').removeClass('bg-dark').removeClass('navbar-dark').addClass('bg-light').addClass('navbar-light');
+
+			$('.card').removeClass('dark');
+			$('.card-title').removeClass('dark');
+			$('.card-text').removeClass('dark');
+		}
+	});
+
 	$(".tag_button").on("click", function(e) {
 		$(".tag_button").removeClass('btn-primary').removeClass('btn-outline-primary').addClass('btn-outline-primary');
 		$(".post").hide();
